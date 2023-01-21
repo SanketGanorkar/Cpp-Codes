a10e3f3be34dbe97727abbfe9e3f6f7ff55a5567
SanketGanorkar <110564177+SanketGanorkar@users.noreply.github.com>
2023-01-21 12:41:11 +0530

    Create swapFunction.cpp

---
+++ Basics/swapFunction.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Swap-Swapping two number in a programming language means exchanging the values of two variables.
+    int a = 10;
+    int b = 20;
+    cout<<"THe value of a before is : "<<a<<"\n";
+    cout<<"THe value of b before is : "<<b<<"\n";
+    swap(a,b);
+    cout<<"THe value of a before is : "<<a<<"\n";
+    cout<<"THe value of b before is : "<<b<<"\n";
+
+    return 0;
+}
